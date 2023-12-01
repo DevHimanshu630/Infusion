68f4d0eb5c07766672207e87f3a82ec351df6a06
Himanshu singh <123050257+DevHimanshu630@users.noreply.github.com>
2023-12-01 14:20:01 +0530

    Main section ready

---
+++ src/components/Main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+// import images from './digital1.jpg';
+function Main() {
+
+const imagePath = 'digital1.jpg'; // Replace with the actual path to your image
+
+  const myStyles = {
+    backgroundImage: `url('${imagePath}')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    WebkitTextFillColor: 'transparent',
+    WebkitBackgroundClip: 'text',
+  };
+  const centerDiv = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  };
+  return (
+    <div className='flex flex-col mt-9' style={centerDiv}>
+        <div className='flex justify-start absolute mt-[70px] text-gray-800 text-[40px]  font-medium leading-125 tracking-wider'>
+            Manage
+        </div>
+        
+        <div className=''>
+            <h1 className='font-poppins text-[334px] font-bold  uppercase' style={myStyles}>digital</h1>
+        </div>
+
+        <div className='flex justify-end absolute mt-[380px] ml-[850px] text-gray-800 text-[40px]  font-medium leading-125 tracking-wider'>
+            Signage with Ease
+        </div>
+
+        <div className='flex gap-5 mt-[90px]'>
+            
+                <p className='text-gray-400 text-[30px] leading-150 tracking-tighter'>The ultimate solution to streamline your signage operations</p>
+                
+                <div className=' flex items-center text-center justify-center w-[225.163px] h-[59px] p-[31.306px 57.796px] gap-[12.041px] border-[1.204px] border-orange-500 shadow-lg'>
+                    <button className='text-orange-500 text-[21.673px] font-semibold leading-125 tracking-wider'>Schedule my Demo</button>
+                </div>
+                <div className=' flex items-center text-center justify-center w-[225.163px] h-[59px] p-[31.306px 57.796px] gap-[12.041px] border-[1.204px] bg-[#FF6500]'>
+                     <button className='text-[#fff] text-[21.673px] font-semibold leading-125 tracking-wider'>Start Free Trial</button>
+                </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex items-center justify-around w-full mt-9'>
+        <div className=''>
+            <h2 className='text-orange-500 text-[40px] font-normal leading-125'>Infusion</h2>
+        </div>
+
+        <div className=''>
+            <ul className='flex gap-9 text-gray-700 text-center text-[20px] font-normal leading-125'>
+                <li>Solutions</li>
+                <li>Pricing</li>
+                <li>Resources</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        <div className=' flex justify-center items-center w-[225px] h-[59px] text-center px-5 bg-orange-500 shadow-lg'>
+            <h2 className=' text-white text-base font-semibold leading-125 tracking-wider'>Start Free Trial</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
